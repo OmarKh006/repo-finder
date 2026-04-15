eb16b79e09aabf3cff38129aa1ff1b94c4d4ad8a
OmarKh006 <khairyomar006@gmail.com>
2026-04-15 12:03:28 +0200

    finishing the setSearchResult function

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-const submitButton = document.querySelector("button");
+const submitButton = document.querySelector(".button");
+const searchInput = document.querySelector(".input");
+const checkUser = document.querySelector(`input[value="users"]`);
+const cards = document.querySelector(".cards");
+
 const API = "https://api.github.com/search/users?q=";
 
 const setSearchResult = (response) => {
-  console.log(response.items[0].login);
+  let result = "";
+
+  response.items.map((item) => {
+    result += `
+    <article class="card">
+        <img class="img" loading="lazy" src="${item.avatar_url}" />
+        <a class="profileLink" href="${item.html_url}" target="_blank">
+            <h2 class="name">${item.login}</h2>
+        </a>
+    </article>
+    `;
+  });
+  cards.innerHTML = result;
 };
 
 const performSearch = (searchTerm, isUser) => {
@@ -10,9 +26,11 @@ const performSearch = (searchTerm, isUser) => {
   fetch(`${API}${searchTerm}${query}`)
     .then((result) => result.json())
     .then((response) => setSearchResult(response));
+  //response.items
 };
 
 submitButton?.addEventListener("click", (e) => {
   e.preventDefault();
-  performSearch("Omarkh006", true);
+
+  performSearch(searchInput.value, checkUser.checked);
 });
