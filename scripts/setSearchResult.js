f487720ce8965196796ee10f096e4a4a1441c4b7
OmarKh006 <khairyomar006@gmail.com>
2026-04-15 13:09:06 +0200

    handling if no results, Finishing the project

--- scripts/setSearchResult.js
+++ scripts/setSearchResult.js
@@ -1,9 +1,11 @@
-import * as element from "./elements";
+import { cards } from "./elements";
 
 export const setSearchResult = (response) => {
   let result = "";
 
-  if (response === null) {
+  if (response?.items?.length === 0) {
+    result = "<p>Not found!</p>";
+  } else if (response === null) {
     result = "";
   } else if (response?.items?.length) {
     response.items.map((item) => {
@@ -17,5 +19,5 @@ export const setSearchResult = (response) => {
     `;
     });
   }
-  element.cards.innerHTML = result;
+  cards.innerHTML = result;
 };
